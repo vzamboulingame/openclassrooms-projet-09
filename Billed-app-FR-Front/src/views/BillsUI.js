b3528ef0f8ff28f7ab6a07a8c6c3f3ae5790346a
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-03-13 16:28:02 +0100

    Fixed bug on Bills.js with incorrect date order :
    - Added chronological order sort on the expected tested dates
    - Added chronological order sort on the data used for bills rendering

--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -1,11 +1,11 @@
-import VerticalLayout from './VerticalLayout.js'
-import ErrorPage from "./ErrorPage.js"
-import LoadingPage from "./LoadingPage.js"
+import VerticalLayout from "./VerticalLayout.js";
+import ErrorPage from "./ErrorPage.js";
+import LoadingPage from "./LoadingPage.js";
 
-import Actions from './Actions.js'
+import Actions from "./Actions.js";
 
 const row = (bill) => {
-  return (`
+  return `
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
@@ -16,16 +16,22 @@ const row = (bill) => {
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
-  }
+    `;
+};
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
-}
+  if (data && data.length) {
+    // sort the data by date in chronological order
+    data.sort((a, b) => new Date(a.date) - new Date(b.date));
+    // map through the sorted data and return the rows
+    return data.map((bill) => row(bill)).join("");
+  } else {
+    return "";
+  }
+};
 
 export default ({ data: bills, loading, error }) => {
-  
-  const modal = () => (`
+  const modal = () => `
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -40,15 +46,15 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
     </div>
-  `)
+  `;
 
   if (loading) {
-    return LoadingPage()
+    return LoadingPage();
   } else if (error) {
-    return ErrorPage(error)
+    return ErrorPage(error);
   }
-  
-  return (`
+
+  return `
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -75,6 +81,5 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
       ${modal()}
-    </div>`
-  )
-}
+    </div>`;
+};
